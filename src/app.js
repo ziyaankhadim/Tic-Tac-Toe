e418cfc5047d161233443d0509ea8f5390baf2bc
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-07-19 08:53:55 +0500

    add function to check for win

--- src/app.js
+++ src/app.js
@@ -9,3 +9,26 @@ let isgameover = false;
 const changeTurn = () => {
   return turn === "X" ? "0" : "X";
 };
+// Function to check for a win
+const checkWin = () => {
+  let boxtext = document.getElementsByClassName("boxtext");
+  let wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  wins.forEach((e) => {
+    if (
+      boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[0]].innerText !== ""
+    ) {
+      isgameover = true;
+    }
+  });
+};
