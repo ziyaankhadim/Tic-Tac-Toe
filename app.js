854458e9a0bffd05875ec07e18a8d9aecb27bdc4
Ziyaan Khadim <ziyaankhadim@gmail.com>
2023-07-19 23:37:20 +0500

    delete source folder

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 "use strict";
 console.log(`Welcome! to Tic Tac Toe Game`);
-let ting = new Audio("../audio/ting.mp3");
-let music = new Audio("../audio/music.mp3");
-let gameover = new Audio("../audio/gameover.mp3");
+let ting = new Audio("./audio/ting.mp3");
+let music = new Audio("./audio/music.mp3");
+let gameover = new Audio("./audio/gameover.mp3");
 let turn = "X";
 let isgameover = false;
 // Function to change the turn
